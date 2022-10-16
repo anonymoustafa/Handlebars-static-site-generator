5c4c6d10dc6aab8d80523ac77811d65bbfd945f9
anonymoustafa <anonymoustafa@outlook.com>
2022-10-16 12:27:30 +0330

    put everything in the root folder

--- root-directory/blog/js/app.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", function (event) {
-    var getTemplateAsString = document.getElementById("h1-template").textContent
-    var compiledContent = Handlebars.compile(getTemplateAsString)
-    document.getElementById("h1-template").innerHTML = compiledContent({template:"apple"})
-});
-
