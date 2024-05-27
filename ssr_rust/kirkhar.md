16a02fdcf7a6a5e45052f9c7c3ee6976405e6312
Mostafa Ramezani <anonymoustafa@outlook.com>
2024-05-27 10:50:07 -0400

    now reads md and writes html :)

---
+++ ssr_rust/kirkhar.md
@@ -0,0 +1,23 @@
+# Markdown to HTML Rust Code Test
+
+This is a test document to check the functionality of your Markdown to HTML Rust code.
+
+## Features to Test
+
+- Basic Markdown formatting:
+  - Headings
+  - Lists (ordered and unordered)
+  - Emphasis (italic and bold)
+  - Links
+  - Images
+- Code blocks
+- Blockquotes
+- Horizontal rules
+
+### Code Example
+
+```rust
+fn main() {
+    println!("Hello, Markdown to HTML Rust code test!");
+}
+
--- ssr_rust/src/main.rs
+++ ssr_rust/src/main.rs
@@ -1,13 +1,19 @@
+use std::io;
+use std::io::Write;
+use std::fs;
 use std::fs::File;
-use std::io::{self, Write};
-
 fn main() -> io::Result<()> {
+    let file_path = "kirkhar.md";
+    let contents = fs::read_to_string(file_path)?;
+
+    // Print the file contents
+    
+
     // Open a file in write-only mode, creating it if it doesn't exist
-    let mut file = File::create("output.txt")?;
+    let mut file = File::create("output.html")?;
 
     // Use write! macro to write formatted string to the file
-    writeln!(file, "Hello, world!")?;
-    writeln!(file, "This is another line.")?;
+    writeln!(file, "{}", markdown::to_html(&contents))?;
     
     // If everything went well, return Ok
     Ok(())
