39f1c7fc629ebb978ed77fc370c6aa31bb954ab3
Mostafa Ramezani <anonymoustafa@outlook.com>
2024-05-30 16:18:15 -0400

    added rust gitignore

--- ssr_rust/target/debug/deps/ssr_rust-8792e7c798302e30.d
+++
@@ -1,5 +0,0 @@
-/home/mostafa/Documents/local-repositories/Handlebars-static-site-generator/ssr_rust/target/debug/deps/libssr_rust-8792e7c798302e30.rmeta: src/main.rs
-
-/home/mostafa/Documents/local-repositories/Handlebars-static-site-generator/ssr_rust/target/debug/deps/ssr_rust-8792e7c798302e30.d: src/main.rs
-
-src/main.rs:
